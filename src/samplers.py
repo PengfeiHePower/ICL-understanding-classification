5c2a50eb1ceda3294f60b56aa79541de2e194fc4
PengfeiHePower <billhe@yahoo.com>
2024-04-10 03:30:42 +0900

    modified

--- src/samplers.py
+++ src/samplers.py
@@ -14,6 +14,7 @@ class DataSampler:
 def get_data_sampler(data_name, n_dims, **kwargs):
     names_to_classes = {
         "gaussian": GaussianSampler,
+        "mix_gaussian": MixGaussianSampler
     }
     if data_name in names_to_classes:
         sampler_cls = names_to_classes[data_name]
@@ -37,6 +38,31 @@ class GaussianSampler(DataSampler):
     def __init__(self, n_dims, bias=None, scale=None):
         super().__init__(n_dims)
         self.bias = bias
+        self.scale = scale
+
+    def sample_xs(self, n_points, b_size, n_dims_truncated=None, seeds=None):
+        if seeds is None:
+            xs_b = torch.randn(b_size, n_points, self.n_dims)
+        else:
+            xs_b = torch.zeros(b_size, n_points, self.n_dims)
+            generator = torch.Generator()
+            assert len(seeds) == b_size
+            for i, seed in enumerate(seeds):
+                generator.manual_seed(seed)
+                xs_b[i] = torch.randn(n_points, self.n_dims, generator=generator)
+        if self.scale is not None:
+            xs_b = xs_b @ self.scale
+        if self.bias is not None:
+            xs_b += self.bias
+        if n_dims_truncated is not None:
+            xs_b[:, :, n_dims_truncated:] = 0
+        return xs_b
+
+
+class MixGaussianSampler(DataSampler):
+    def __init__(self, n_dims, bias=2.0, scale=None):
+        super().__init__(n_dims)
+        self.bias = bias = torch.normal(mean=bias, std=1.0, size=(1, n_dims))
         self.scale = scale
 
     def sample_xs(self, n_points, b_size, n_dims_truncated=None, seeds=None):
